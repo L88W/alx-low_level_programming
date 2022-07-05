de7a08a3d1112a7f5cf3618609f507509c56ca59
L88W <lukewelman8@gmail.com>
2022-07-05 05:30:24 -0700

    ya

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
+ *main - Print _putchar
  *
- * *main - Print _putchar
+ *Return: Always 0(Success)
  *
- * *
- *
- * *Return: Always 0(Success)
- *
- * */
+ */
 
 int main(void)
-
 {
-	_putchar(95);
-	_putchar(112);
-	_putchar(117);
-	_putchar(116);
-	_putchar(99);
-	_putchar(104);
-	_putchar(97);
-	_putchar(114);
-	_putchar('\n');
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,5 @@
-int putchar() ;
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
