deb41117a5abe5ade55d8a49f88b95302dfa1ef4
L88W <lukewelman8@gmail.com>
2022-07-05 15:21:15 -0700

    sauce

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,12 @@
 void print_alphabet_x10(void)
 {
 	int coun = 0;
-
 	char alpha;
 
 	while (coun++ <= 9)
 	{
 		for (alpha = 97; alpha <= 122; alpha++)
-			_putchar (alpha);
-		_putchar ('\n');
+			_putchar(alpha);
+		_putchar('\n');
 	}
 }
