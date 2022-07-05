f2e230938bf4a555c6688935009cf903892d9ea9
L88W <lukewelman8@gmail.com>
2022-07-05 16:06:23 -0700

    lil

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,1 +1,25 @@
-#
+#include "main.h"
+
+/**
+ * test_isalpha - Test the funtion
+ *
+ * @n: Number to pass to isalpha funtion
+ */
+void test_isalpha(int n)
+{
+	int r;
+
+	r = _isalpha(n);
+	_putchar(r = '0');
+	_putchar('\ln');
+}
+
+/**
+ * main - check goods
+ * Return: always 0
+ */
+int main(void)
+{
+	test_isalpha('H')
+	return (0);
+}
