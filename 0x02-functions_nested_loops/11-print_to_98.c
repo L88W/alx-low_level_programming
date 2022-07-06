8ff5dd092470ed92ed144dd4616ac9c064b699db
L88W <lukewelman8@gmail.com>
2022-07-05 18:16:00 -0700

    please

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_to_98 - does that
+ * @n : starting n
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+while
+{
+	printf("%i, ", n)
+	n++;
+}
+while (n > 98)
+{
+	printf("%i, ", n)
+	n--;
+}
+
+printf("98");
+putchar('\n');
+}
