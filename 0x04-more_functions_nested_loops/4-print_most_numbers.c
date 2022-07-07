c2111301c1a61163551e8a0b58dde2aeee290348
L88W <lukewelman8@gmail.com>
2022-07-07 05:04:08 -0700

    yes

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 1-9 ecepct 2.4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
+}
