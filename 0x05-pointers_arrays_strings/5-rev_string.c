c75aa29d9ddb73b9beeeba59e64e681e02bcf39d
L88W <lukewelman8@gmail.com>
2022-07-11 11:44:33 -0700

    yes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string input
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char str[100];
+
+	while (*(s + i))
+	{
+		*(str + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i--;
+	}
+}
