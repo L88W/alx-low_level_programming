dba57ee374efa7c5069281c0d1846a0e76d8fb7f
L88W <lukewelman8@gmail.com>
2022-07-13 23:56:25 -0700

    yeas sauce

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - copies string
+ * @dest: destination
+ * @src: source
+ * @n: bytes
+ * Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
