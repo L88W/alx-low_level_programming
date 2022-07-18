1dbee268b5d4403755d8ad03058e1a12751f8678
L88W <lukewelman8@gmail.com>
2022-07-18 08:11:35 -0700

    yes

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets pointer to char
+ * @s: pointer changes
+ * @to: string to chanfge
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
