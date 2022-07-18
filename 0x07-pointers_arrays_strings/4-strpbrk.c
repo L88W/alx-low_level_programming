6449534ab46a3b9a46dc03762ef9e1b9218b87c3
L88W <lukewelman8@gmail.com>
2022-07-18 07:34:02 -0700

    yes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*8
+ * _strbrk - searches bytes in string
+ * @s: first prestent
+ * @accept: matches bytes o r null if non
+ * Return: pointer to byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+		while (*s)
+		{
+			for (i = 0; accept[i]; i++)
+			{
+				if (*s == accept[i])
+					return (s);
+			}
+			s++;
+		}
+	return (NULL);
+}
