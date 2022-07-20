42623fd5d01a1a68c80e135f2c73ca246bcd7cc5
L88W <lukewelman8@gmail.com>
2022-07-20 12:47:38 -0700

    yes

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,3 +15,17 @@ int find_root(int n, int i)
 	else
 		return (-1);
 }
+/**
+ * _sqrt_recursion - calculates nautural square root of n
+ * @n: base number
+ *
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (find_root(n, 2));
+}
