470c8205409020c4ec5e09e5a38a836a76b8a8e9
L88W <lukewelman8@gmail.com>
2022-07-28 09:46:46 -0700

    yes

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/*8
+/**
  * malloc_checked - terminatioation value of 98
  * @b: allocated memory
  * Return: 0
@@ -9,7 +9,8 @@ void *malloc_checked(unsigned int b)
 	void *i;
 
 	i = malloc(b);
+
 	if (i == NULL)
-	       exit(98);
+		exit(98);
 	return (i);
-}	
+}
