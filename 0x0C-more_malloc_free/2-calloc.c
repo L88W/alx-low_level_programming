95209e1d21bd800b3ccad33184529461947411e2
L88W <lukewelman8@gmail.com>
2022-07-28 10:20:57 -0700

    yes

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _calloc - memeory for array of @nemb
+ * @nmeemb: allocates memory for arry
+ * @size: aloocates element of size
+ *
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int b;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+	for (b = 0; b < (nmemb * size); b++)
+		a[b] = 0;
+	return (a);
+}
