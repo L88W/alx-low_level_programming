0b0f0381f10af042acb4da679de0b5abd782cfe3
L88W <lukewelman8@gmail.com>
2022-08-01 01:58:40 -0700

    yes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG.H
+#define DOG.H
+
+/**
+ * struct dog - structure of dog class
+ * @name: name of dog
+ * @age: age od og
+ * @owner: woner of dog
+ */
+struct dog
+{
+	char *name;
+	int *age;
+	char *owner;
+}
+#endif /* DOG */
+
