62ef3b73a8419715883f4d754e6aef5a085a6b3c
L88W <lukewelman8@gmail.com>
2022-08-01 05:53:43 -0700

    yes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#ifndef DOG
+#ifndef DOGi
 #define DOG
 
 /**
@@ -10,9 +10,9 @@
 struct dog
 {
 	char *name;
-	int *age;
+	float age;
 	char *owner;
-}
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
