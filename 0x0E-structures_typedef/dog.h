3df557c3798cfb875625788f482d719dd34d2338
L88W <lukewelman8@gmail.com>
2022-08-01 03:43:34 -0700

    yes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,17 @@ struct dog
 	char *name;
 	int *age;
 	char *owner;
-}
-#endif /* DOG */
+};
+
+oid init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif /* DOG.H */
 
